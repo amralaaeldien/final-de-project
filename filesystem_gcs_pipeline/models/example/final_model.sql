363510de9adcd244b6b14d2b81ab82e88841ce6d
‪Amr Mahmoud‬‏ <amrshedou@gmail.com>
2025-03-19 00:51:32 +0000

    final.2

---
+++ filesystem_gcs_pipeline/models/example/final_model.sql
@@ -0,0 +1,26 @@
+{{ config(materialized='table') }}
+
+with source_data as (
+
+    select 
+          date
+          customer_id,
+           c.city,
+           category,
+           product,
+          unit_price,
+           quantity,
+           sales_amount,
+           used_discount,
+           discount_amount,
+           final_sales ,
+           latitude,
+           longitude   
+    
+     as first_city from {{ ref("coffee_model") }} as c inner join
+    {{ref("saudi_model")}} as s on c.city = s.city
+
+)
+
+select *
+from source_data
